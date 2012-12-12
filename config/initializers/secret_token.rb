e4530665c548a2ec29db5016ec2a24c41bf7581a
kotohata-imac <t.kotohata@gmail.com>
2012-12-12 16:58:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddOnRails::Application.config.secret_token = '58989e9bb270359a0a18ce11189358cf6f4952b95029e1b80217404a431f22c066d0f9ebc52fc549a4f65988907d7094aec62cd744051395e5e238ce2d4bc783'
